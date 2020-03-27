bf10926cf053f29a42f809ef7c5ed5cc2ca0ee8a
chenzh66 <chenzh66@midea.com>
2020-03-27 20:19:49 +0800

    提交轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,18 @@
 Page({
   data: {
-   
+    background: [{
+        url: 'http://img4.imgtn.bdimg.com/it/u=280538008,469035030&fm=26&gp=0.jpg',
+        des: '111'
+      },
+      {
+        url: 'http://img4.imgtn.bdimg.com/it/u=1664133777,2632814243&fm=26&gp=0.jpg',
+        des: '222'
+      },
+      {
+        url: 'http://img5.imgtn.bdimg.com/it/u=2486233385,2229376018&fm=26&gp=0.jpg',
+        des: '333'
+      }
+    ],
   },
-  
+
 })
